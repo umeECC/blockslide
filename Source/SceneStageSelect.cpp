20554045190af90bc2b31f202a186b2785620fcc
Anpontanmaturi <2240070@ecc.ac.jp>
2025-05-23 14:32:10 +0900

    Update SceneStageSelect.cpp

--- Source/SceneStageSelect.cpp
+++ Source/SceneStageSelect.cpp
@@ -9,6 +9,17 @@
 OBJ2D select_back;
 int stage_number;
 
+// キーが押されていたかどうか
+bool wasAKeyPressed = false;
+bool wasDKeyPressed = false;
+
+// 長押し判定用のタイマー（ミリ秒 or フレーム数）
+int AKeyHoldTimer = 0;
+int DKeyHoldTimer = 0;
+
+// ホールド時のインターバル
+int holdInterval = 10;
+
 void SceneSelect::init()
 {
 	timer = 0;
@@ -28,26 +39,70 @@ void SceneSelect::update()
     using namespace GameLib::input;
     // 現在のキーの状態を取得
     bool isAKeyPressed = (GetAsyncKeyState('A') & 0x8000) != 0;
-    bool isDKeyPressed = (GetAsyncKeyState('D') & 0x8000) != 0;
+    bool isDKeyPressed = (GetAsyncKeyState('D') & 0x8000) != 0;    
 
-    // キーが押されていたかどうか
-    bool wasAKeyPressed = false;
-    bool wasDKeyPressed = false;
-
-    // Wキーが押された瞬間を判定
-    if (isAKeyPressed && !wasAKeyPressed)
+    // Aキーが押された瞬間を判定
+    /*if (isAKeyPressed && !wasAKeyPressed)
     {
         AudioManager::getInstance().playSound(L"selectSound", 0.5f, false);
         stage_number--;
-        if (stage_number < -1) stage_number = 0;
+        if (stage_number <= -1) stage_number = 0;
+    }*/
+    if (isAKeyPressed)
+    {
+        if (!wasAKeyPressed)
+        {
+            // 押された瞬間
+            AudioManager::getInstance().playSound(L"selectSound", 0.5f, false);
+            stage_number--;
+            if (stage_number < -1) stage_number = -1;
+
+            AKeyHoldTimer = 0; // タイマーリセット
+        }
+        else
+        {
+            // 長押し中
+            AKeyHoldTimer++;
+            if (AKeyHoldTimer >= holdInterval)
+            {
+                AudioManager::getInstance().playSound(L"selectSound", 0.5f, false);
+                stage_number--;
+                if (stage_number < -1) stage_number = -1;
+                AKeyHoldTimer = 0; // タイマーリセットして連続反応させる
+            }
+        }
+    }
+    else
+    {
+        AKeyHoldTimer = 0; // 離したらタイマーリセット
     }
 
-    // Sキーが押された瞬間を判定
-    if (isDKeyPressed && !wasDKeyPressed)
+    // Dキーが押された瞬間を判定
+    if (isDKeyPressed)
     {
-        AudioManager::getInstance().playSound(L"selectSound", 0.5f, false);
-        stage_number++;
-        if (stage_number >= 10) stage_number = 1;
+        if (!wasDKeyPressed)
+        {
+            AudioManager::getInstance().playSound(L"selectSound", 0.5f, false);
+            stage_number++;
+            if (stage_number > 10) stage_number = 10;
+
+            DKeyHoldTimer = 0;
+        }
+        else
+        {
+            DKeyHoldTimer++;
+            if (DKeyHoldTimer >= holdInterval)
+            {
+                AudioManager::getInstance().playSound(L"selectSound", 0.5f, false);
+                stage_number++;
+                if (stage_number > 10) stage_number = 10;
+                DKeyHoldTimer = 0;
+            }
+        }
+    }
+    else
+    {
+        DKeyHoldTimer = 0;
     }
 
     // 現在の状態を次のフレームのために保存
@@ -65,4 +120,8 @@ void SceneSelect::draw()
     GameLib::clear(0, 0, 0);
 
     select_back.draw();
+
+    std::string StageNumber = std::to_string(stage_number);
+    GameLib::font::textOut(4, StageNumber, { SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2 }, { 10, 10 },
+        { 1, 1, 0, 1 }, GameLib::TEXT_ALIGN::MIDDLE, 0.0f, false);
 }
