c288369494824400b7a9360def429dc61c5aab52
Anpontanmaturi <2240070@ecc.ac.jp>
2025-05-27 16:46:57 +0900

    iranaimonokeshita

--- Source/SceneEnd.cpp
+++ Source/SceneEnd.cpp
@@ -6,18 +6,13 @@
 #include "Obj2d.h"
 #include "Audio.h"
 
-int SOelpos;
 VECTOR2 pos2;
-bool wasWKeyPressed = false;
-bool wasSKeyPressed = false;
 
 OBJ2D end_back;
-OBJ2D end_back2;
 
 void SceneEnd::init()
 {
     timer = 0;
-    SOelpos = 0;
     pos2 = { 90,250 };
 
     // テクスチャのロード
@@ -42,42 +37,13 @@ void SceneEnd::deinit()
 
 void SceneEnd::update()
 {
-    using namespace GameLib::input;
-    // 現在のキーの状態を取得
-    bool isWKeyPressed = (GetAsyncKeyState('W') & 0x8000) != 0;
-    bool isSKeyPressed = (GetAsyncKeyState('S') & 0x8000) != 0;
-
-    // Wキーが押された瞬間を判定
-    if (isWKeyPressed && !wasWKeyPressed)
+    using namespace GameLib::input;    
+    
+    if (TRG(0) & PAD_START)
     {
-        AudioManager::getInstance().playSound(L"selectSound", 0.5f, false);
-        SOelpos--;
-        if (SOelpos < 0) SOelpos = 1;
-    }
-
-    // Sキーが押された瞬間を判定
-    if (isSKeyPressed && !wasSKeyPressed)
-    {
-        AudioManager::getInstance().playSound(L"selectSound", 0.5f, false);
-        SOelpos++;
-        if (SOelpos >= 2) SOelpos = 0;
-    }
-
-    // 現在の状態を次のフレームのために保存
-    wasWKeyPressed = isWKeyPressed;
-    wasSKeyPressed = isSKeyPressed;
-
-    switch (SOelpos)
-    {
-    case 0:
-        pos2 = { 90,440 };
-        if (TRG(0) & PAD_START)
-        {
-            AudioManager::getInstance().playSound(L"btnSound", 0.5f, false);
-            setScene(SCENE::GAME);
-        }
-        break;
-    }
+        AudioManager::getInstance().playSound(L"btnSound", 0.5f, false);
+        setScene(SCENE::GAME);
+    }    
 
     AudioManager::getInstance().update();
 
@@ -87,19 +53,5 @@ void SceneEnd::update()
 void SceneEnd::draw()
 {
     GameLib::clear(0, 0, 0);
-
-
-    switch (SOelpos)
-    {
-    case 0:
-
-        end_back.draw();
-
-        break;
-    case 1:
-
-        end_back2.draw();
-
-        break;
-    }
+    end_back.draw();
 }
