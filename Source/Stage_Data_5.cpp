b6dee4c86ce3372610d934c9c1b765d1e821c320
umeECC <umeda2240@gmail.com>
2025-06-02 13:30:59 +0900

    a

--- Source/Stage_Data_5.cpp
+++ Source/Stage_Data_5.cpp
@@ -27,20 +27,35 @@ StageScript stage5Script[] = {
     SET_OBJ(1, 288,  88, set_wall, TYPE_WALL),
     SET_OBJ(1, 928,  88, set_wall, TYPE_WALL),
     SET_OBJ(1, 800, 152, set_hako, TYPE_WALL),
+    SET_OBJ(1,800,216,set_Toge,TYPE_WALL),
     SET_OBJ(1, 864, 152, set_wall, TYPE_WALL),
     SET_OBJ(1,800,216,set_invwall,TYPE_WALL),
     SET_OBJ(1,928,152,set_wall,TYPE_WALL),
-    //SET_OBJ(1, 800, 152, set_wall, TYPE_WALL),
+   // SET_OBJ(1, 800, 152, set_wall, TYPE_WALL),
+    SET_OBJ(1, 288, 152, set_wall, TYPE_WALL),
+
     SET_OBJ(1, 288, 216, set_wall, TYPE_WALL),
     
     SET_OBJ(1, 928, 216, set_wall, TYPE_WALL),
-    SET_OBJ(1, 288, 280, set_wall, TYPE_WALL),
+   // SET_OBJ(1, 288, 280, set_wall, TYPE_WALL),
     
     SET_OBJ(1, 928, 280, set_wall, TYPE_WALL),
     SET_OBJ(1, 288, 344, set_wall, TYPE_WALL),
     
     SET_OBJ(1, 928, 344, set_wall, TYPE_WALL),
     SET_OBJ(1, 288, 408, set_wall, TYPE_WALL),
-    
+     SET_OBJ(1, 352, 584, set_wall, TYPE_WALL),
+    SET_OBJ(1, 416, 584, set_wall, TYPE_WALL),
+    SET_OBJ(1, 480, 584, set_wall, TYPE_WALL),
+    SET_OBJ(1, 544, 584, set_wall, TYPE_WALL),
+    SET_OBJ(1, 608, 584, set_wall, TYPE_WALL),
+    SET_OBJ(1, 672, 584, set_wall, TYPE_WALL),
+    SET_OBJ(1, 736, 584, set_wall, TYPE_WALL),
+    SET_OBJ(1, 800, 584, set_wall, TYPE_WALL),
+    SET_OBJ(1, 864, 584, set_wall, TYPE_WALL),
+    SET_OBJ(1, 288, 584, set_wall, TYPE_WALL),
+    SET_OBJ(1, 928, 584, set_wall, TYPE_WALL),
+    SET_OBJ(1,288,520,set_wall,TYPE_WALL),
+    SET_OBJ(1,288,456,set_wall,TYPE_WALL),
 
 };
